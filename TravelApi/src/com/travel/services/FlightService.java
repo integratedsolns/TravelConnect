2a0d7c282bb5ef94ff273882eb48f8b1d952abbd
Elias Tadesse <melkamguzo@yahoo.com>
2015-12-30 14:16:15 -0600

    First Commit

---
+++ TravelApi/src/com/travel/services/FlightService.java
@@ -0,0 +1,11 @@
+package com.travel.services;
+
+public class FlightService {
+
+	public static void main(String[] args) {
+		System.out.println("Flight Service class");
+		System.out.println("In Repository");
+
+	}
+
+}
