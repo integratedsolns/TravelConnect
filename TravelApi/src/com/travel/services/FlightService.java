c36261093ae6a79eb0c4eceaa26029fae99f2cd4
Elias <melkamguzo@yahoo.com>
2015-12-30 14:29:04 -0600

    Second commit

--- TravelApi/src/com/travel/services/FlightService.java
+++ TravelApi/src/com/travel/services/FlightService.java
@@ -1,7 +1,15 @@
 package com.travel.services;
 
+
+
 public class FlightService {
 
+	
+	public String getBookingStatus (String boookingCode)
+	{
+		return "Pending";
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Flight Service class");
 		System.out.println("In Repository");
